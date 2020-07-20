8a8c1f8f7d59ed03ffe5fe8404baddd5fe28d9a6
Zach <zjbiles@ut-bizaz440-centos.cliffsnet.com>
2020-07-20 15:33:46 -0500

    updated for hostname modificatiions

--- import_prtg.py
+++ import_prtg.py
@@ -37,33 +37,26 @@ user = config.get('prtg', 'prtg_user')
 password = config.get('prtg', 'prtg_passhash')
 # PRTG Tag
 tag = config.get('prtg', 'prtg_tag')
-# Field for groups
-groupField = 'GroupName'
-# Field for host
-hostField = 'SysName'
+# FQDN for hostname manipulation
+domain = config.get('prtg', 'prtg_domain').lower()
 
 # string to be attached to the server url on what to pull back from PRTG.
-search_payload = "content=devices&columns=objid,device,status,name,active,host,group,tags&filter_tags=@tag(" + \
-    tag+")&username="+user+"&passhash="+password+""
+search_payload = "content=devices&columns=objid,device,status,name,active,host,group,tags&filter_tags=@tag("+tag+")&username="+user+"&passhash="+password+""
 
 # headers
 headers = {'Content-Type': 'application/yang-data+json',
-           'Accept': 'application/yang-data+json'}
+            'Accept': 'application/yang-data+json'}
 
 # beginning of API URL
 url = "https://"+server+"/api/table.json?"
 api_search_string = url + search_payload
-req = requests.get(api_search_string, params=search_payload,
-                   headers=headers, verify=False)
+req = requests.get(api_search_string, params=search_payload, headers=headers, verify=False)
 
 jsonget = req.json()
 devices = jsonget["devices"]
 
-#use_groups = False
-
-
 class prtgInventory(object):
-    # CLI parameters
+    #CLI parameters
     def read_cli(self):
         parser = argparse.ArgumentParser()
         parser.add_argument('--host')
@@ -77,9 +70,6 @@ class prtgInventory(object):
         # Called with `--list`.
         if self.args.list:
             self.inventory = self.get_list()
-            # if use_groups:
-            #     self.groups = self.get_groups()
-            #     self.add_groups_to_hosts(self.groups)
         # Called with `--host [hostname]`.
         elif self.args.host:
             # Not implemented, since we return _meta info `--list`.
@@ -94,14 +84,10 @@ class prtgInventory(object):
     def get_list(self):
         hostsData = jsonget
 
-        # unsure if this is needed
-        # data_dump = eval(json.dumps(jsonget))
-
         # Inject data below to speed up script
         final_dict = {'_meta': {'hostvars': {}}}
-        # use the one below to overwrite an existing static inventory
+        #use the one below to overwrite an existing static inventory
         #final_dict = {'_meta': {'hostvars': {}}, 'all': {'children': ['ungrouped']}}
-        #final_dict = {}
 
         # Loop hosts in groups and remove special chars from group names
         for m in hostsData['devices']:
@@ -110,21 +96,75 @@ class prtgInventory(object):
                 # Allow Upper/lower letters and numbers. Replace everything else with underscore
                 m["name"] = self.clean_inventory_item(m["name"])
                 m["group"] = self.clean_group_name(m["group"])
+                #if self.validate_host_string((m["name"])) is True:
                 if m["group"] in final_dict:
                     final_dict[m["group"]]['hosts'].append(m["name"])
                 else:
                     final_dict[m["group"]] = {'hosts': [m["name"]]}
-            # add groups to all
-            # m["group"]['children'].append(m["group"])
         return final_dict
 
     # cleanup hostnames so they are proper
     @staticmethod
     def clean_inventory_item(item):
+        # register regex string for valid FQDN
+        regex = re.compile(r'(?=^.{4,253}$)(^((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}$)')
+
+        #if ip has no hostname don't add the domain to the end
+        result = re.match(r'^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$', item)
+        if result:
+            return item
+
+
         # check for FQDN format, and replace anything else with underscore
-        item = re.sub('[^A-Za-z0-9\-\.]+', '_', item)
-        item = item.replace(" ", "")
-        return item
+        if domain.casefold() in item.casefold():
+            # check for match that hostname is already in proper format
+            result = re.match(r'^.+[A-Za-z0-9\-\.]\.[A-Za-z]+$', item)
+            if result:
+                return item.upper()
+            else:
+                #cleanup the hostname string
+                item = re.sub('[_]+', '-', item)
+                item = item.replace(" ","")
+                # check if there is a properly formatted hostname in ()
+                result = re.match(r'.*([\(])([A-Za-z0-9\-\.]+\.[A-Za-z]+[^\)])', item)
+                if result:
+                    # if hostname is in (), process the string and return it
+                    # try new regex below item = re.findall(r'(^.+[A-Za-z0-9\-\.]\.[A-Za-z]+)', item)
+                    item = re.findall(r'(.+\(([A-Za-z0-9\-\.]+\.[A-Za-z]+[^\)]))', item)
+                    item = item[0][1]
+                    #check if valid FQDN
+                    result = re.match(regex, item)
+                    if result:
+                        return item.upper()
+                else:
+                    # if hostname is at the beginning, process the string and return it
+                    item = re.findall(r'(^[A-Za-z0-9\-\.]+\.[A-Za-z]+)', item)
+                    item = item[0]
+                    #check if valid FQDN
+                    result = re.match(regex, item)
+                    if result:
+                        return item.upper()
+        else:
+            #cleanup the hostname string
+            item = re.sub('[^A-Za-z0-9\-\.]+', '_', item)
+            item = item.replace(" ","")
+            # add domain to the string and try and parse it
+            item = item + "." + domain
+            # check if valid FQDN
+            result = re.match(regex, item)
+            if result:
+                return item.upper()
+            else:
+                return "bad_hostname_delete_me"
+                print(item + " is not a proper FQDN hostname")
+
+    @staticmethod
+    def validate_host_string(item):
+        # regex to check for FQDN
+        fqdn_result = re.search(r'(?=^.{4,253}$)(^((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}$)', item)
+        if fqdn_result:
+            result = process(fqdn_result)
+            return bool(result)
 
     # cleanup whitespace in group names
     @staticmethod
@@ -142,7 +182,6 @@ class prtgInventory(object):
         parser.add_argument('--list', action='store_true')
         parser.add_argument('--host', action='store')
         self.args = parser.parse_args()
-
 
 # Get the inventory.
 prtgInventory()
